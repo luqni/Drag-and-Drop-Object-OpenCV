f71e10f43fc15a581a4c526bb4fac63e7d0ccd40
luqni <mluqnibaehaqi@gmail.com>
2021-08-29 20:23:53 +0700

    Bismillah

---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = HandDetector(detectionCon=0.8)
+colorR = 255, 0, 255
+
+cx, cy, w, h = 50, 50, 100, 100
+
+
+class DragReact():
+    def __init__(self,posCenter, size=[100,100]):
+        self.posCenter = posCenter
+        self.size = size
+
+    def update(self,cursor):
+        cx, cy = self.posCenter
+        w,h = self.size
+
+        # jika indek jari berada di area kotak
+        if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+            self.posCenter = cursor
+
+rect = DragReact([150, 150])
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    img = detector.findHands(img)
+    lmList, _ = detector.findPosition(img)
+
+    if lmList:
+        l, _, _ = detector.findDistance(8, 12, img)
+        print(l)
+        if l < 30:
+            cursor = lmList[8] # indek jari
+            # call the update here
+            rect.update(cursor)
+
+    ## Draw
+    cx, cy = rect.posCenter
+    w, h = rect.size
+    cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
+
+    cv2.imshow("Gambar", img)
+    cv2.waitKey(1)
